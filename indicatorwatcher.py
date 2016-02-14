dcc14821d6b18941b4039b432bd4b05928d566e2
Mike Fang <mike_fang@trend.com.tw>
2016-02-15 00:05:07 +0800

    1. Complete the flow of indicatorwatcher 2. Fix main.py typo 3. Fix the example xpath in the data_config.json 4. Modify the requirement

--- indicatorwatcher.py
+++ indicatorwatcher.py
@@ -1,10 +1,41 @@
 import json
+import requests
+import urllib2
+from lxml import etree
 from bs4 import BeautifulSoup
-import request
 
 class IndicatorWatcher(object):
-    def __init__(self, indecator_config):
-        pass
+    def __init__(self, indicator_config):
+        with open(indicator_config) as f:
+            self.config_data = json.load(f)
+
+    
 
     def execute(self):
-        raise NotImplementedError("")
+        result_list = []
+        for web in self.config_data:
+            url = web['url']
+            indicators = web['indicators']
+            response = urllib2.urlopen(url)
+            htmlparser = etree.HTMLParser()
+            tree = etree.parse(response, htmlparser)
+            for indicator in indicators:
+                xpath = indicator['xpath']
+                find_list = tree.xpath(xpath)
+                if find_list:
+                    try:
+                        value = float(find_list[0].text)
+                        condition = indicator['condition']
+                        full_condition = str(value) + condition
+                        if eval(full_condition):
+                            name = indicator['name']
+                            message = indicator['message']
+                            result_list.append({'name': name, 'value': value, 'message': message})
+                        
+                    except ValueError:
+                        print "Value error, %s is not a digit" % find_list[0].text
+                    except BaseException as e:
+                        print "Unexpected Error. %s" % str(e)
+                else:
+                    print "It doesn't find anything based on the given xpath"
+        return result_list
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from indicatorwatcher import IndicatorWatcher
 from notifier import Notifier
 if __name__ == "__main__":
     to_be_notified = IndicatorWatcher("data_config.json").execute()
+    print to_be_notified
     if to_be_notified:
-        Notifier(to_be_nofied, 'receiver_config.json').send()
+        Notifier(to_be_notified, 'receiver_config.json').send()
 
 
 
