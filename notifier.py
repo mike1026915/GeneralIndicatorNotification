f7791a32740e4817196ef0036a6f267e576f2909
Chuck Lin <chuck42315@gmail.com>
2016-02-15 21:36:03 +0800

    Notifier prototype

--- notifier.py
+++ notifier.py
@@ -1,7 +1,23 @@
+import json
+from push_methods import SEND_METHOD
+
+class NotifierFactory(object):
+    @staticmethod
+    def getSender(push_method):
+        if push_method in SEND_METHOD:
+            return SEND_METHOD[push_method]
+        else:
+            raise NotImplementedError("This push method is not registered.")
+
 
 class Notifier(object):
-    def __init__(self, to_be_notify, receiver_config):
-        pass
+    def __init__(self, receiver_config):
+        self._receiver_config = self._parse_config(receiver_config)
 
-    def send(self):
-        raise NotImplementedError("")
+    def _parse_config(self, receiver_config):
+        raise NotImplementedError("Todo")
+
+    def process_queue(self, message_queue):
+        # msg
+        # NotifierFactory.getSender(method)(msg)
+        raise NotImplementedError("Todo")
---
+++ push_methods.py
@@ -0,0 +1,7 @@
+def qpush_send(msg):
+    pass
+
+def pushbullet_send(msg):
+    pass
+
+SEND_METHOD = {"qpush":qpush_send, "pushbullet":pushbullet_send}
