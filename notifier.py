8e6e9eda187b7ab1e55910ee45ec92a4dcb49dca
Chuck Lin <chuck42315@gmail.com>
2016-02-16 23:56:23 +0800

    1. Add user class 2. Implement Notifer 3. Write sample code for push methods

--- notifier.py
+++ notifier.py
@@ -1,5 +1,6 @@
 import json
 from push_methods import SEND_METHOD
+from user import User
 
 class NotifierFactory(object):
     @staticmethod
@@ -12,12 +13,33 @@ class NotifierFactory(object):
 
 class Notifier(object):
     def __init__(self, receiver_config):
+        self._indicator_watchers = dict()
+        self._users = list()
         self._receiver_config = self._parse_config(receiver_config)
 
     def _parse_config(self, receiver_config):
-        raise NotImplementedError("Todo")
+        with open(receiver_config) as config_file:
+            json_array = json.load(config_file)
+        for user_obj in json_array:
+            user = User(user_obj)
+            self._users.append(user)
+
+            for indicator in user.monitor_indicators:
+                if indicator not in self._indicator_watchers:
+                    self._indicator_watchers[indicator] = list()
+                self._indicator_watchers[indicator].append(user)
+
+        print self._indicator_watchers
 
     def process_queue(self, message_queue):
-        # msg
-        # NotifierFactory.getSender(method)(msg)
-        raise NotImplementedError("Todo")
+        msg_strs = dict()
+        for user in self._users:
+            msg_strs[user] = ""
+
+        for msg in message_queue:
+            for user in self._indicator_watchers[msg['name']]:
+                msg_strs[user] += msg['message'] + "\n"
+
+        for user, msg in msg_strs.iteritems():
+            if msg != "":
+                NotifierFactory.getSender(user.push_method)(user, msg)
--- push_methods.py
+++ push_methods.py
@@ -1,7 +1,11 @@
-def qpush_send(msg):
-    pass
+def qpush_send(user, msg):
+    print "Qpush"
+    print "Send to :" + user.name
+    print "msg : {" + msg + "}"
 
-def pushbullet_send(msg):
-    pass
+def pushbullet_send(user, msg):
+    print "Pushbullet"
+    print "Send to :" + user.name
+    print "msg : {" + msg + "}"
 
 SEND_METHOD = {"qpush":qpush_send, "pushbullet":pushbullet_send}
